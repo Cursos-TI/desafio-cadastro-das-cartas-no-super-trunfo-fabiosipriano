a71a2817870e406f89f61384326260c53248b4a0
Fabio <fabfloriano@hotmail.com>
2025-06-17 20:38:14 +0000

    finalizado nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,16 +4,17 @@
 
 int main() {
     // Definição de variáveis
-    int populacao1, numeropontoturistico1, populacao2, numeropontoturistico2;
-    float area1, pib1, area2, pib2, densidade1, pibpercapta1, densidade2, pibpercapta2;
+    unsigned long int populacao1,populacao2; 
+    int numeropontoturistico1, numeropontoturistico2;
+    float area1, pib1, area2, pib2, densidade1, pibpercapta1, densidade2, pibpercapta2, superpoder1, superpoder2;
     char codigocarta1[3], nomecidade1[20], estado1[20], codigocarta2[3], nomecidade2[20], estado2[20];
     // Cadastro das Cartas:
     printf("Iniciando cadastro da primeira carta\nInsira o codigo da carta:\n");
-    scanf("%s", &codigocarta1);       
+    scanf("%s", codigocarta1);       
     printf("Insira o ESTADO da carta:\n");
-    scanf("%s", &estado1);
+    scanf("%s", estado1);
     printf("Insira o nome da cidade:\n");
-    scanf("%s", &nomecidade1);
+    scanf("%s", nomecidade1);
     printf("Insira a população:\n");
     scanf("%d", &populacao1);
     printf("Insira a área em km²:\n");
@@ -23,11 +24,11 @@ int main() {
     printf("Insira o número de pontos turisticos:\n");
     scanf("%d", &numeropontoturistico1);
     printf("\nIniciando cadastro da segunda carta\nInsira o código da carta:\n");
-    scanf("%s", &codigocarta2);       
+    scanf("%s", codigocarta2);       
     printf("Insira o ESTADO da carta:\n");
-    scanf("%s", &estado2);
+    scanf("%s", estado2);
     printf("Insira o nome da cidade:\n");
-    scanf("%s", &nomecidade2);
+    scanf("%s", nomecidade2);
     printf("Insira a população:\n");
     scanf("%d", &populacao2);
     printf("Insira a área em km²:\n");
@@ -41,6 +42,8 @@ int main() {
     densidade2 = (float) populacao2 / area2;
     pibpercapta1 = (float) pib1 / populacao1;
     pibpercapta2 = (float) pib2 / populacao2;
+    superpoder1 = (float) populacao1 + area1 + pib1 + numeropontoturistico1 + pibpercapta1 + (1/densidade1);
+    superpoder2 = (float) populacao2 + area2 + pib2 + numeropontoturistico2 + pibpercapta2 + (1/densidade2);
     // Exibição dos Dados das Cartas:
     printf("\nCartas cadastradas\nCarta 1\n");
     printf("\nEstado: %s", estado1);
@@ -62,6 +65,14 @@ int main() {
     printf("\nNúmero de pontos turisticos: %d", numeropontoturistico2);
     printf("\nDensidade Populacional: %.2f", densidade2);
     printf("\nPIB per capta: %.2f\n", pibpercapta2);
-    
+    printf("\nExibição do resultado das comparações:\n");
+    printf("\nPopulação: %d\n", populacao1 > populacao2);
+    printf("Área: %d\n", area1 > area2);
+    printf("PIB: %d\n", pib1 > pib2);
+    printf("Pontos Turísticos: %d\n", numeropontoturistico1 > numeropontoturistico2);
+    printf("Densidade Populacional: %d\n", densidade1 < densidade2);
+    printf("PIB per capta: %d\n", pibpercapta1 > pibpercapta2);
+    printf("Super Poder: %d\n", superpoder1 > superpoder2);
+
     return 0;
 }
